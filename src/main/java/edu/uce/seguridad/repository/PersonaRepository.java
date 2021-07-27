4e37f2b83ac9025f9c5dd4a115b2f3e6a9b22f1b
alpalasaul <alpalasaul@gmail.com>
2021-07-26 22:35:45 -0500

    Nuevas consultas sin usar query dentro de PersonaRepository

--- src/main/java/edu/uce/seguridad/repository/PersonaRepository.java
+++ src/main/java/edu/uce/seguridad/repository/PersonaRepository.java
@@ -9,17 +9,22 @@ import java.util.List;
 public interface PersonaRepository extends MongoRepository<Persona, String> {
     @Query("{'usuario.nombreUsuario': ?0, 'usuario.contrasena': ?1}")
     Persona findPersonaByUsuarioYContrasena(String nombreUsuario, String contrasena);
+//    Persona findByUsuarioNombreUsuarioAndUsuarioContrasena(String nombreUsuario, String contrasena);
 
     List<Persona> findPersonaByOrganizacion(String organizacion);
 
     @Query("{'usuario.role': ?0}")
     List<Persona> findPersonaByRole(String role);
+//    List<Persona> findByUsuarioRole(String role);
 
     @Query("{'usuario.role': ?0, 'organizacion': ?1}")
     List<Persona> findPersonaByRolAndOrganization(
             String role, String organization
     );
+//    List<Persona> findByUsuarioRoleAndOrganizacion(String role, String organizacion);
 
     @Query("{'usuario.nombreUsuario': ?0}")
     Persona findPersonaByUsuario(String nombreUsuario);
+//    Persona findByUsuarioNombreUsuario(String nombreUsuario);
+
 }
