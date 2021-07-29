8fcd9ec7ee8da16d5e6f7e806dac1d25ab862c5a
Erick E. Diaz P <erickdp@hotmail.com>
2021-07-28 19:52:34 -0500

    Mas puertos

--- src/main/java/edu/uce/seguridad/controller/ControladorPersona.java
+++ src/main/java/edu/uce/seguridad/controller/ControladorPersona.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/sgcnegocio")
-@CrossOrigin(origins = {"http://localhost:3000"},
+@CrossOrigin(origins = {"http://localhost:3***", "http://localhost:80**"},
         methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @AllArgsConstructor
 public class ControladorPersona {
