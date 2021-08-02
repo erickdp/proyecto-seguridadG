0d0684cc278f1525f9940392bedcef20e0d6dc6d
Erick E. Diaz P <erickdp@hotmail.com>
2021-08-01 19:26:00 -0500

    Fixed Resolve

--- src/test/java/edu/uce/seguridad/controller/ControladorPersonaTest.java
+++ src/test/java/edu/uce/seguridad/controller/ControladorPersonaTest.java
@@ -3,17 +3,10 @@ package edu.uce.seguridad.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.uce.seguridad.service.service.PersonaService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.omg.CORBA.PRIVATE_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static edu.uce.seguridad.data.DatosPersona.getPersona001;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @WebMvcTest
 class ControladorPersonaTest {
@@ -25,4 +18,9 @@ class ControladorPersonaTest {
     MockMvc mvc;
 
     private ObjectMapper objectMapper;
+
+    @Test
+    void elimarPersonaTest() {
+
+    }
 }
