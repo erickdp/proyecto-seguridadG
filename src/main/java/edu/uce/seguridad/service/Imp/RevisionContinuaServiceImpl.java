6933104dd975e65f6845f0710ed11a04eb6cf5e6
alpalasaul <alpalasaul@gmail.com>
2021-09-26 12:00:07 -0500

    pdf resumen 10.1 pcn

--- src/main/java/edu/uce/seguridad/service/Imp/RevisionContinuaServiceImpl.java
+++ src/main/java/edu/uce/seguridad/service/Imp/RevisionContinuaServiceImpl.java
@@ -2,14 +2,25 @@ package edu.uce.seguridad.service.Imp;
 
 import edu.uce.seguridad.exception.EliminacionException;
 import edu.uce.seguridad.exception.NoEncontradoExcepcion;
+import edu.uce.seguridad.model.HojaDeRevisionDeGerencia;
 import edu.uce.seguridad.model.RevisionContinua;
 import edu.uce.seguridad.repository.RevisionContinuaRepo;
 import edu.uce.seguridad.service.service.RevisionContinuaService;
 import lombok.AllArgsConstructor;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JsonDataSource;
+import org.json.JSONObject;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -61,5 +72,36 @@ public class RevisionContinuaServiceImpl implements RevisionContinuaService {
             this.repository.delete(revision);
             estadoCompletadoService.verificarEstadoPaso10(revision.getUsuario());
         }
+    }
+
+    @Override
+    public byte[] generarPdfEnBytes(String user) throws IOException, JRException {
+        RevisionContinua data = this.repository.findRevisionByUsuario(user);
+        InputStream resource = new ClassPathResource("resumen.jrxml").getInputStream();
+
+        List<JSONObject> dataJson = new ArrayList<>();
+        if (data != null) {
+
+            data.getTemas().forEach(var -> {
+                JSONObject aux = new JSONObject();
+                aux.put("paso", var.getPaso());
+                aux.put("temaRevVer", var.getTemaRevVer());
+                aux.put("formulario", var.getFormulario());
+                aux.put("estado", var.getEstado());
+                aux.put("cambios", var.getCambios());
+                aux.put("temasRevisar", var.getTemasRevisar());
+
+                dataJson.add(aux);
+            });
+
+            ByteArrayInputStream jsonDataStream = new ByteArrayInputStream(dataJson.toString().getBytes());
+            JsonDataSource ds = new JsonDataSource(jsonDataStream);
+            JasperReport jasperReport = JasperCompileManager.compileReport(resource);
+            Map<String, Object> map = new HashMap<>();
+            map.put("createdBy", "sgcn");
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, map, ds);
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        }
+        return null;
     }
 }
--- src/main/java/edu/uce/seguridad/service/service/RevisionContinuaService.java
+++ src/main/java/edu/uce/seguridad/service/service/RevisionContinuaService.java
@@ -2,8 +2,13 @@ package edu.uce.seguridad.service.service;
 
 import edu.uce.seguridad.exception.NoEncontradoExcepcion;
 import edu.uce.seguridad.model.RevisionContinua;
+import net.sf.jasperreports.engine.JRException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public interface RevisionContinuaService extends BaseService<RevisionContinua, String> {
     RevisionContinua buscarRevisionPorUsuario(String nombreUsuario) throws NoEncontradoExcepcion;
     void eliminarRespuestaFormularioRevisionContinua(String nombreUsuario);
+    byte[] generarPdfEnBytes(String user) throws IOException, JRException;
 }
